28df1fd9deeef8ce9fd354beed0f41d4c088607e
postwo <codo7717@naver.com>
2024-12-01 14:36:37 +0900

    ã세팅

---
+++ src/test/java/com/example/fcss_05/Fcss05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fcss_05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fcss05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
